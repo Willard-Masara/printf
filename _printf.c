4ea810a5f970f600e4b98e91a2089b79fa0e11c4
Lulekoh <lulekoleoquintus12@gmail.com>
2023-06-27 06:41:18 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,52 +1,77 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+
+void print_buffer(char buffer[], int *buff_ind);
 /**
- * put_string - The string to evaluate/read though.
- * @str: string to input
- * Return: void
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 
-void put_string(const char *str)
+int _printf(const char *format, ...)
 {
-while (*str != '\0')
-putchar (*str);
-str++;
+
+        int i, printed = 0, printed_chars = 0;
+
+        int flags, width, precision, size, buff_ind = 0;
+
+        va_list list;
+
+        char buffer[BUFF_SIZE];
+
+        if (format == NULL)
+
+                return (-1);
+
+        va_start(list, format);
+
+        for (i = 0; format && format[i] != '\0'; i++)
+        {
+                if (format[i] != '%')
+                {
+                        buffer[buff_ind++] = format[i];
+
+                        if (buff_ind == BUFF_SIZE)
+                                print_buffer(buffer, &buff_ind);
+                        /* write(1, &format[i], 1);*/
+                        printed_chars++;
+                }
+
+                else
+                {
+                        print_buffer(buffer, &buff_ind);
+                        flags = get_flags(format, &i);
+                        width = get_width(format, &i, list);
+                        precision = get_precision(format, &i, list);
+                        size = get_size(format, &i);
+                        ++i;
+                        printed = handle_print(format, &i, list, buffer,
+                                flags, width, precision, size);
+
+                        if (printed == -1)
+                                return (-1);
+                        printed_chars += printed;
+                }
+        }
+
+        print_buffer(buffer, &buff_ind);
+        va_end(list);
+        return (printed_chars);
 }
+
 /**
- * our_printf - initialize to move pass special sign
- * @format: format
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-void our_printf(const char *format, ...)
+
+void print_buffer(char buffer[], int *buff_ind)
 {
-va_list args;
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-}
-if (*format == 'c')
-{
-int ch = va_arg(args, int);
-putchar(ch);
-}
-else if (*format == 's')
-{
-char *str = va_arg(args, char *);
-puts(str);
-}
-else if (*format == '%')
-{
-putchar('%');
-}
-else
-{
-putchar(*format);
-}
-format++;
-}
-va_end(args);
+
+        if (*buff_ind > 0)
+
+                write(1, &buffer[0], *buff_ind);
+
+
+        *buff_ind = 0;
+
 }
